0a3dd4b9f9ff1dcdc99a692ffaae2326f8a864e3
ESDUNCAN <d.eric@rogers.com>
2022-05-02 19:59:28 +0000

    update

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log("❌❌❌Assertion Passed: " + actual + " === " + expected);
+  } else
+    console.log("😃😃😃Assertion Failed: " + actual + " !== " + expected);
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
