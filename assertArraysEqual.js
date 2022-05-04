3844cf6bb68fe3b9abad2da528249ad848ff5c11
ESDUNCAN <d.eric@rogers.com>
2022-05-04 00:16:10 +0000

    update

---
+++ assertArraysEqual.js
@@ -0,0 +1,26 @@
+const eqArrays = require("./eqArrays")
+
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`😃😃😃Assertion Passed: ${actual} === ${expected}`);
+  } else
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+};
+
+let assertArraysEqual = function (arr1, arr2) {
+  let equalCondition = false
+  if (Array.isArray(arr1) && Array.isArray(arr2)) {
+    equalCondition = eqArrays(arr1, arr2)
+  } if (equalCondition === true) {
+    console.log(`😃😃😃Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+}
+
+console.log(assertArraysEqual([1, 2, 3], [1, 2, 3])) // => true
+console.log(assertArraysEqual([1, 2, 3], [3, 2, 1])) // => false
+
+console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"])) // => true
+console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", 3])) // => false
+console.log(assertEqual(assertArraysEqual([1, 2, 3], [1, 2, 3]), true))
--- eqArrays.js
+++ eqArrays.js
@@ -20,3 +20,4 @@ console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
 console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
 console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
 console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true))
+module.exports = eqArrays
