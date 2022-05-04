06a4fa0649467b401e9f9200926f6fe0d1c237ce
ESDUNCAN <d.eric@rogers.com>
2022-05-04 00:55:35 +0000

    update

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -24,3 +24,4 @@ console.log(assertArraysEqual([1, 2, 3], [3, 2, 1])) // => false
 console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"])) // => true
 console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", 3])) // => false
 console.log(assertEqual(assertArraysEqual([1, 2, 3], [1, 2, 3]), true))
+module.exports = assertArraysEqual
