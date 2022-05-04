926502bb689b9f464eea7ea6f595721af5a475d1
ESDUNCAN <d.eric@rogers.com>
2022-05-04 20:15:37 +0000

    update

---
+++ countLetters.js
@@ -0,0 +1,21 @@
+const assertEqual = require("./assertEqual")
+
+const countLetters = function (letters) {
+  const answer = {}
+  for (const key of letters) {
+    if (key !== " ") {
+      if (answer[key]) {
+        answer[key] += 1
+      }
+      else {
+        answer[key] = 1
+      }
+    }
+  } return answer
+}
+
+
+console.log(countLetters('You are the best Thank You very much'))
+
+
+
