b5233696d9c6166c8bb769e931786f4184f8cd5a
ESDUNCAN <d.eric@rogers.com>
2022-05-05 03:08:07 +0000

    update

--- eqArrays.js
+++ eqArrays.js
@@ -6,6 +6,7 @@ const assertEqual = function (actual, expected) {
 };
 
 
+
 function eqArrays(arr1, arr2) {
   let answer = true
   for (let i = 0; i < arr1.length; i++) {
@@ -14,10 +15,10 @@ function eqArrays(arr1, arr2) {
     }
   } return answer
 }
-console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+//console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+//console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
 
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
-console.log(assertEqual(eqArrays([1, 3, 3], [1, 2, 3]), true))
+//console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+//console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+//console.log(assertEqual(eqArrays([1, 3, 3], [1, 2, 3]), true))
 module.exports = eqArrays
