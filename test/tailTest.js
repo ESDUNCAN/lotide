d27227b9486a7a7ba57a73146ef9f1fc4a9c2768
ESDUNCAN <d.eric@rogers.com>
2022-05-09 20:39:17 +0000

    update

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,4 +1,15 @@
 const tail = require('../tail')
+const assert = require('chai').assert;
 
 
-console.log(tail(["Hello", "Lighthouse", "Labs"])); // returns (all elements in array except the first one)
+describe("#tail", () => {
+  it("returns [2, 3] for [1,2,3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it("returns [Lighthouse, Labs] for [Hello, Lighthouse, Labs]", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+});
+
+
+// console.log(tail(["Hello", "Lighthouse", "Labs"])); // returns (all elements in array except the first one)
